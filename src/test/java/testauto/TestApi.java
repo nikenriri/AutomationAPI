41a86e8ea6b7414ff007c2ed55276ea995285bb0
Niken Riri <nikenriri05@gmail.com>
2026-05-22 15:37:54 +0700

    First commit

---
+++ src/test/java/testauto/TestApi.java
@@ -0,0 +1,168 @@
+package testauto;
+
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class TestApi {
+    String token;
+    @BeforeClass
+    public void testLoginUser(){
+        Response hasil =
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body("""
+                        { "email": "nikenriri05@gmail.com",
+                          "password" : "150500Niken*"
+                        }
+                      """)
+                .when()
+                .post("https://api.rizqifauzan.com/api/auth/login");
+
+        hasil.then().log().all();
+
+        token = hasil.jsonPath().getString("data.token");
+    }
+
+    @Test
+    public void testGetAllUser(){
+        File jsonScema = new File("src/test/resources/jsonScema/getListUserScema.json");
+
+        RestAssured
+                .given()
+                .auth()
+                .oauth2(token)
+                .when()
+                .get("https://api.rizqifauzan.com/api/siswa")
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonScema));
+    }
+
+    @Test
+    public void testGetSpecificUser(){
+        File jsonScema = new File("src/test/resources/jsonScema/getUserById.json");
+
+        String userId = "24905351-4daf-4c50-ab00-f3e052f64e95";
+
+        RestAssured
+                .given()
+                .auth()
+                .oauth2(token)
+                .when().get("https://api.rizqifauzan.com/api/siswa/" + userId)
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonScema));
+    }
+
+    @Test
+    public void testPostCeateUser(){
+        String valueName = "Dewi Sartika";
+        String valueNis = "124200001";
+        String valueKelas = "XI-IPS-2";
+        String valueJurusan = "IPS";
+        String valueEmail = "dewi.05@gmail.com";
+        String valueTelp = "085890344142";
+        String valueAlamat = "Semarang";
+
+        JSONObject bodyObj = new JSONObject();
+
+        bodyObj.put("nama", valueName);
+        bodyObj.put("nis", valueNis);
+        bodyObj.put("kelas", valueKelas);
+        bodyObj.put("jurusan", valueJurusan);
+        bodyObj.put("email", valueEmail);
+        bodyObj.put("telepon", valueTelp);
+        bodyObj.put("alamat", valueAlamat);
+
+        RestAssured.given()
+                .auth()
+                .oauth2(token)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(bodyObj.toString())
+                .when()
+                .post("https://api.rizqifauzan.com/api/siswa")
+                .then().log().all()
+                .assertThat().statusCode(201)
+                .assertThat().body("data.nama", Matchers.equalTo(valueName));
+    }
+
+    @Test
+    public void testPutUpdateUser(){
+        String valueName = "Widiany Putri Ramadhani";
+        String valueNis = "124200090";
+        String valueKelas = "XI-IPA-3";
+        String valueJurusan = "IPA";
+        String valueEmail = "widiany01@gmail.com";
+        String valueTelp = "08123450534";
+        String valueAlamat = "Jaksel";
+
+        JSONObject bodyObj = new JSONObject();
+
+        bodyObj.put("nama", valueName);
+        bodyObj.put("nis", valueNis);
+        bodyObj.put("kelas", valueKelas);
+        bodyObj.put("jurusan", valueJurusan);
+        bodyObj.put("email", valueEmail);
+        bodyObj.put("telepon", valueTelp);
+        bodyObj.put("alamat", valueAlamat);
+
+        RestAssured.given()
+                .auth()
+                .oauth2(token)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(bodyObj.toString())
+                .when()
+                .put("https://api.rizqifauzan.com/api/siswa/24905351-4daf-4c50-ab00-f3e052f64e95")
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body("data.nama", Matchers.equalTo(valueName));
+    }
+
+    @Test
+    public void testPatchUpdateUser(){
+        String userId = "24905351-4daf-4c50-ab00-f3e052f64e95";
+        String valueEmail = "widiany.putri@gmail.com";
+
+        JSONObject bodyObj = new JSONObject();
+
+        bodyObj.put("email", valueEmail);
+
+        RestAssured.given()
+                .auth()
+                .oauth2(token)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(bodyObj.toString())
+                .when()
+                .patch("https://api.rizqifauzan.com/api/siswa/" + userId)
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body("data.email", Matchers.equalTo(valueEmail));
+    }
+
+    @Test
+    public void testDeleteUser(){
+
+        String userToDelete = "bdb49d4c-dc36-4011-b46a-c62026ceaa45";
+
+        RestAssured
+                .given()
+                .auth()
+                .oauth2(token)
+                .when()
+                .delete("https://api.rizqifauzan.com/api/siswa/" + userToDelete)
+                .then().log().all()
+                .assertThat().statusCode(200);
+    }
+
+}
